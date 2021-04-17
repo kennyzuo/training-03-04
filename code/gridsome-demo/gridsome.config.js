abec8583b31f7500a5f2ea00f60d04db545b3305
kennyzuo <353715843@qq.com>
2021-04-17 23:01:23 +0800

    功能完善

--- code/gridsome-demo/gridsome.config.js
+++ code/gridsome-demo/gridsome.config.js
@@ -5,6 +5,38 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Gridsome',
-  plugins: []
+  siteName: "Gridsome",
+  plugins: [
+    {
+      use: "@gridsome/source-filesystem",
+      options: {
+        typeName: "BlogPost",
+        path: "./content/blog/**/*.md",
+      },
+    },
+    {
+      use: "@gridsome/source-strapi",
+      options: {
+        apiURL: "http://localhost:1337",
+        queryLimit: 1000, // Defaults to 100
+        contentTypes: ["post"],
+        // typeName: 'Strapi'
+        // singleTypes: ["impressum"],
+        // Possibility to login with a Strapi user,
+        // when content types are not publicly available (optional).
+        loginData: {
+          identifier: "zheng@qq.com",
+          password: "Kenny@513",
+        },
+      },
+    },
+  ],
+  templates: {
+    StrapiPost: [
+      {
+        path: '/post/:id',
+        component: './src/templates/Post.vue'
+      }
+    ]
+  }
 }
--- code/gridsome-demo/src/pages/Index.vue
+++ code/gridsome-demo/src/pages/Index.vue
@@ -19,85 +19,63 @@
     <div class="container">
       <div class="row">
         <div class="col-lg-8 col-md-10 mx-auto">
-          <div class="post-preview">
-            <a href="post.html">
+          <div class="post-preview" v-for="edge in $page.posts.edges" :key="edge.node.id">
+            <g-link :to="'/post/' + edge.node.id">
               <h2 class="post-title">
-                Man must explore, and this is exploration at its greatest
+                {{ edge.node.title }}
               </h2>
-              <h3 class="post-subtitle">
-                Problems look mighty small from 150 miles up
-              </h3>
-            </a>
+            </g-link>
             <p class="post-meta">
               Posted by
-              <a href="#">Start Bootstrap</a>
-              on September 24, 2019
+              <a href="#">{{ edge.node.owner ? edge.node.owner.username : '' }}</a>
+              on {{ edge.node.created_at }}
+            </p>
+            <p>
+              <a href="" v-for="tag in edge.node.tags" :key="tag.id">{{ tag.title }}</a>
             </p>
           </div>
-          <hr />
-          <div class="post-preview">
-            <a href="post.html">
-              <h2 class="post-title">
-                I believe every human has a finite number of heartbeats. I don't
-                intend to waste any of mine.
-              </h2>
-            </a>
-            <p class="post-meta">
-              Posted by
-              <a href="#">Start Bootstrap</a>
-              on September 18, 2019
-            </p>
-          </div>
-          <hr />
-          <div class="post-preview">
-            <a href="post.html">
-              <h2 class="post-title">
-                Science has not yet mastered prophecy
-              </h2>
-              <h3 class="post-subtitle">
-                We predict too much for the next year and yet far too little for
-                the next ten.
-              </h3>
-            </a>
-            <p class="post-meta">
-              Posted by
-              <a href="#">Start Bootstrap</a>
-              on August 24, 2019
-            </p>
-          </div>
-          <hr />
-          <div class="post-preview">
-            <a href="post.html">
-              <h2 class="post-title">
-                Failure is not an option
-              </h2>
-              <h3 class="post-subtitle">
-                Many say exploration is part of our destiny, but it’s actually
-                our duty to future generations.
-              </h3>
-            </a>
-            <p class="post-meta">
-              Posted by
-              <a href="#">Start Bootstrap</a>
-              on July 8, 2019
-            </p>
-          </div>
+          
           <hr />
           <!-- Pager -->
-          <div class="clearfix">
-            <a class="btn btn-primary float-right" href="#"
-              >Older Posts &rarr;</a
-            >
-          </div>
+          <pager :info="$page.posts.pageInfo" />
         </div>
       </div>
     </div>
   </Layout>
 </template>
-
+<page-query>
+# Write your query or mutation here
+query($page: Int) {
+  posts: allStrapiPost (perPage: 2, page: $page) @paginate{
+    pageInfo{
+      totalPages
+      currentPage
+    }
+    edges {
+      node{
+        id,
+        title,
+        owner {
+          username,
+          id
+        },
+        tags {
+          id,
+          title
+        },
+        created_at
+      }
+    }
+  }
+}
+</page-query>
 <script>
+import { Pager } from 'gridsome';
 export default {
   name: "HomePage",
+  components: {
+    Pager
+  },
   metaInfo: {
     title: "Hello, world!",
   },
